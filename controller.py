f998826530d724dae30e8afedd29f78049c8c681
Nicha RUANGRIT <nicha.rua@ku.th>
2024-02-19 10:30:32 +0700

    fixed evaluate_expression function

--- controller.py
+++ controller.py
@@ -3,19 +3,19 @@ from model import History
 
 
 class Calculator:
-    def __init__(self, history, ui):
+    def __init__(self, history=None, ui=None):
         self.history = history
         self.ui = ui
-        self.ui.set_cgitontroller(self)
+        # self.ui.set_cgitontroller(self)
 
-    def update_display(self, result):
-        self.ui.display_text(result)
+    # def update_display(self, result):
+    #     self.ui.display_text(result)
 
     def calculate(self, expression):
         try:
             result = eval(expression)
             self.history.add_to_history(expression, result)
-            self.update_display(result)
+            # self.update_display(result)
         except Exception as e:
             pass
 
@@ -23,12 +23,11 @@ class Calculator:
         current_text = self.ui.widget.get_display_text()
         self.ui.display_text(current_text[:-1])
 
-    def clear_function(self):
-        self.update_display("")
+    def evaluate_expression(self, expression: str):
+        # exp = self.ui.get_display_text()
+        # print(exp)
+        try:
+            return eval(expression)
+        except Exception as e:
+            return e
 
-    def evaluate_expression(self):
-        exp = self.ui.get_display_text()
-        if exp:
-            self.calculate(exp)
-        else:
-            self.ui.show_error()
