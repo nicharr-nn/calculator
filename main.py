698979143822a26af929fdb640eae94c61820aa1
Nicha RUANGRIT <nicha.rua@ku.th>
2024-02-19 17:13:30 +0700

    edited file

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from view import Calculator_UI
 from controller import Calculator
 
 if __name__ == '__main__':
-    # create the UI.  There is no controller (yet), so nothing to inject.
     calculator = Calculator()
     ui = Calculator_UI(calculator)
     ui.run()
