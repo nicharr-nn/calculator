e54256f82c481586369e6725966c0dcb2e306966
Nicha RUANGRIT <nicha.rua@ku.th>
2024-02-19 14:21:42 +0700

    edited file

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 """Display the calculator user interface."""
-from calculator_ui import Calculator_UI
-
+from view import Calculator_UI
+from controller import Calculator
 
 if __name__ == '__main__':
     # create the UI.  There is no controller (yet), so nothing to inject.
-    ui = Calculator_UI()
+    calculator = Calculator()
+    ui = Calculator_UI(calculator)
     ui.run()
