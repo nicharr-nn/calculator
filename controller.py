113ea7e6f7e156fd5df430f24860a9901e0f4ba3
Nicha RUANGRIT <nicha.rua@ku.th>
2024-02-19 12:54:16 +0700

    edited class

--- controller.py
+++ controller.py
@@ -1,4 +1,5 @@
 from model import History
+import math
 # from view import Calculator_UI
 
 
@@ -6,28 +7,14 @@ class Calculator:
     def __init__(self, history=None, ui=None):
         self.history = history
         self.ui = ui
-        # self.ui.set_cgitontroller(self)
-
-    # def update_display(self, result):
-    #     self.ui.display_text(result)
-
-    def calculate(self, expression):
-        try:
-            result = eval(expression)
-            self.history.add_to_history(expression, result)
-            # self.update_display(result)
-        except Exception as e:
-            pass
-
-    def del_function(self):
-        current_text = self.ui.widget.get_display_text()
-        self.ui.display_text(current_text[:-1])
 
     def evaluate_expression(self, expression: str):
-        # exp = self.ui.get_display_text()
-        # print(exp)
         try:
-            return eval(expression)
+            result = eval(expression)
+            self.save_model()
+            return result
         except Exception as e:
             return e
 
+    def save_model(self):
+        pass
