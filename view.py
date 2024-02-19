0b1c7be98e1c4a53eaf5f2e6d0e467b45b43378a
Nicha RUANGRIT <nicha.rua@ku.th>
2024-02-19 12:55:34 +0700

    edited and added new method

--- view.py
+++ view.py
@@ -11,8 +11,13 @@ class Calculator_UI:
         self.tk.geometry("700x600")
         self.tk.resizable(True, True)
         self.text_result = tk.StringVar()
+        self.combo_value = tk.StringVar()
         self.controller = controller
         self.stack = []
+
+        self.operator_list = ['*', '/', '+', '-', '^', '=', '(', ')', 'DEL', 'CLR']
+        self.adv_operator_list = ['exp', 'ln', 'log2', 'log10', 'sqrt']
+        self.replace_list = self.adv_operator_list + ['^']
 
         self.init_components()
 
@@ -22,16 +27,17 @@ class Calculator_UI:
         self.display_label = ttk.Label(frame, textvariable=self.text_result, anchor="e", font=('Arial', 14))
         self.display_label.grid(row=0, column=0, columnspan=5, padx=10, pady=5, sticky="news")
 
-        adv_opt = ttk.Combobox(frame, values=['exp', 'ln', 'log2', 'log10', 'sqrt'])
+
+        adv_opt = ttk.Combobox(frame, values=self.adv_operator_list, textvariable=self.combo_value)
         adv_opt.grid(row=1, column=0, padx=5, pady=5, sticky="news", columnspan=4)
         adv_opt.current(0)
-        adv_opt.bind('<<ComboboxSelected>>', self.observer)
+        adv_opt.bind('<<ComboboxSelected>>', self.cbb_text)
 
         keypad = Keypad(frame, keynames=['7', '8', '9', '4', '5', '6', '1', '2', '3', ' ', '0', '.'], columns=3)
         keypad.grid(row=2, column=0, sticky="news", columnspan=3)
         keypad.bind('<Button>', self.observer)
 
-        op = Keypad(frame, keynames=['*', '/', '+', '-', '^', '=', '(', ')', 'DEL', 'CLR'])
+        op = Keypad(frame, keynames=self.operator_list)
         op.grid(row=2, column=3, sticky="news", columnspan=1)
         op.bind('<Button>', self.observer)
 
@@ -51,21 +57,44 @@ class Calculator_UI:
         elif key_pressed == 'CLR':
             self.text_result.set("")
         elif key_pressed == '=':
-            result = self.controller.evaluate_expression(expression=self.text_result.get())
-            print(type(result))
+            txt = self.text_result.get()
+            print(self.replace_list)
+            # Mark for loop useing slicing with index
+            for i in self.replace_list:
+                if i in txt:
+                    if i == '^':
+                        txt = txt.replace(i, '**')
+                    elif i == 'ln':
+                        txt = txt.replace(i, 'math.' + i)
+                    else:
+                        txt = txt.replace(i, 'math.' + i)
+            print(txt)
+            result = self.controller.evaluate_expression(expression=txt)
             if isinstance(result, SyntaxError):
-                print('do this')
                 self.error_msg(result)
             else:
                 self.text_result.set(result)
         else:
             self.text_result.set(self.text_result.get() + event.widget.cget('text'))
+            print(self.text_result.get())
 
     def get_display_text(self):
         return self.text_result.get()
 
     def observer(self, event):
+        operator = self.combo_value.get()
         self.display_text(event)
+
+    def cbb_text(self, event):
+        operator = self.combo_value.get()
+        print('op', operator)
+        if self.text_result.get() == "":
+            self.text_result.set(self.combo_value.get() + '(')
+        else:
+            if self.text_result.get()[-1:] in self.operator_list:
+                self.text_result.set(self.text_result.get() + self.combo_value.get() + '(')
+            else:
+                self.text_result.set(self.combo_value.get() + '(' + self.text_result.get() + ')')
 
     def error_msg(self, result):
         tk.messagebox.showerror("Error Message", result)
